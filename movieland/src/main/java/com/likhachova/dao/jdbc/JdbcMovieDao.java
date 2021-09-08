4910f8c42696b541dcb51d6653ace9347a0228fe
AnnaLikhachova <a.cannsh@gmail.com>
2021-09-08 03:01:56 +0300

    [b-5] added sorting

--- movieland/src/main/java/com/likhachova/dao/jdbc/JdbcMovieDao.java
+++ movieland/src/main/java/com/likhachova/dao/jdbc/JdbcMovieDao.java
@@ -21,6 +21,10 @@ public class JdbcMovieDao implements MovieDao {
     private static final String SELECT_ALL = "SELECT * FROM movie;";
     private static final String SELECT_THREE_RANDOM = "SELECT id, name_russian, name_native, year_of_release, description, rating, price, picture_path, votes FROM movie ORDER BY RAND() limit ?;";
     private static final String SELECT_MOVIES_BY_GENRE = "SELECT movie.id, name_russian, name_native, year_of_release, description, rating, price, picture_path, votes FROM movie JOIN movie_genre ON movie.id = movie_genre.movie_id JOIN genre ON genre.id = movie_genre.genre_id WHERE genre.id = ?;";
+    private static final String SORT_MOVIES_BY_PRICE = "SELECT * FROM movie ORDER BY price %s;";
+    private static final String SORT_MOVIES_BY_RATING = "SELECT * FROM movie ORDER BY rating %s;";
+    private static final String SELECT_MOVIES_BY_GENRE_SORT_BY_PRICE = "SELECT movie.id, name_russian, name_native, year_of_release, description, rating, price, picture_path, votes FROM movie JOIN movie_genre ON movie.id = movie_genre.movie_id JOIN genre ON genre.id = movie_genre.genre_id WHERE genre.id = ? ORDER BY price %s;;";
+    private static final String SELECT_MOVIES_BY_GENRE_SORT_BY_RATING = "SELECT movie.id, name_russian, name_native, year_of_release, description, rating, price, picture_path, votes FROM movie JOIN movie_genre ON movie.id = movie_genre.movie_id JOIN genre ON genre.id = movie_genre.genre_id WHERE genre.id = ? ORDER BY rating %s;;";
 
 
     public JdbcMovieDao(DataSource dataSource) {
@@ -44,6 +48,34 @@ public class JdbcMovieDao implements MovieDao {
     public List<Movie> findByGenre(int genreId) {
         logger.debug("Find movies by genre from database");
         return jdbcTemplate.query(SELECT_MOVIES_BY_GENRE, new MovieRowMapper(), genreId);
+    }
+
+    @Override
+    public List<Movie> sortByPrice(String sortingOrder) {
+        logger.debug("Sort movies by price");
+        String sql = String.format(SORT_MOVIES_BY_PRICE, sortingOrder);
+        return jdbcTemplate.query(sql, new MovieRowMapper());
+    }
+
+    @Override
+    public List<Movie> sortByRating(String sortingOrder) {
+        logger.debug("Sort movies by rating");
+        String sql = String.format(SORT_MOVIES_BY_RATING, sortingOrder);
+        return jdbcTemplate.query(sql, new MovieRowMapper());
+    }
+
+    @Override
+    public List<Movie> findByGenreSortByPrice(int genreId, String sortingOrder) {
+        logger.debug("Find movies by genre and sort movies by price");
+        String sql = String.format(SELECT_MOVIES_BY_GENRE_SORT_BY_PRICE, sortingOrder);
+        return jdbcTemplate.query(sql, new MovieRowMapper(), genreId);
+    }
+
+    @Override
+    public List<Movie> findByGenreSortByRating(int genreId, String sortingOrder) {
+        logger.debug("Find movies by genre and sort movies by rating");
+        String sql = String.format(SELECT_MOVIES_BY_GENRE_SORT_BY_RATING, sortingOrder);
+        return jdbcTemplate.query(sql, new MovieRowMapper(), genreId);
     }
 
 }
--- movieland/src/main/java/com/likhachova/web/controller/MovieController.java
+++ movieland/src/main/java/com/likhachova/web/controller/MovieController.java
@@ -18,22 +18,46 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-    @GetMapping(value ="/api/v1/movie", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Movie> findAll(){
+    @GetMapping(value = "/api/v1/movie", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Movie> findAll() {
         logger.debug("Get all movies from database");
         return movieService.findAll();
     }
 
-    @GetMapping(value ="/v1/movie/random", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Movie> findThreeRandom(){
+    @GetMapping(value = "/v1/movie/random", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Movie> findThreeRandom() {
         logger.debug("Get three random movies from database");
-        return  movieService.findThreeRandom(3);
+        return movieService.findThreeRandom(3);
     }
 
-    @GetMapping(value ="/v1/movie/genre/{genreId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Movie> findByGenre(@PathVariable String genreId){
+    @GetMapping(value = "/v1/movie/genre/{genreId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Movie> findByGenre(@PathVariable String genreId) {
         logger.debug("Get movies by genre from database");
-        return  movieService.findByGenre(Integer.parseInt(genreId));
+        return movieService.findByGenre(Integer.parseInt(genreId));
+    }
+
+    @GetMapping(value = "/v1/movie/genre/{genreId}/price={sortingOrder}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Movie> sortByPriceOrderByGenre(@PathVariable String genreId, @PathVariable String sortingOrder) {
+        logger.debug("Get movies by genre {} and sort them by price {}" + genreId, sortingOrder);
+        return movieService.findByGenreSortByPrice(Integer.parseInt(genreId), sortingOrder);
+    }
+
+    @GetMapping(value = "/v1/movie/genre/{genreId}/rating={sortingOrder}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Movie> sortByRatingOrderByGenre(@PathVariable String genreId, @PathVariable String sortingOrder) {
+        logger.debug("Get movies by genre {} and sort them by rating {}" + genreId, sortingOrder);
+        return movieService.findByGenreSortByRating(Integer.parseInt(genreId), sortingOrder);
+    }
+
+    @GetMapping(value = "/v1/movie?price={sortingOrder}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Movie> sortAllMoviesByPrice(@PathVariable String sortingOrder) {
+        logger.debug("Sort all movies by price {}" + sortingOrder);
+        return movieService.sortByPrice(sortingOrder);
+    }
+
+    @GetMapping(value = "/v1/movie?rating={sortingOrder}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Movie> sortAllMoviesByRating(@PathVariable String sortingOrder) {
+        logger.debug("Sort all movies by rating {}" + sortingOrder);
+        return movieService.sortByRating(sortingOrder);
     }
 
 }
